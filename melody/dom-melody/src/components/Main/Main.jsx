d939e5056b47f8b7078f03e36662da3277a06244
Oleg <fondumik@yandex.ru>
2021-08-21 10:03:04 +0500

    add floor counter

--- melody/dom-melody/src/components/Main/Main.jsx
+++ melody/dom-melody/src/components/Main/Main.jsx
@@ -5,7 +5,19 @@ import { ReactSVG } from 'react-svg'
 export default class Main extends React.Component {
 
   state = {
-    
+    currentFloor: 2,
+    choiceFloorHead: 'Выберите желаемый этаж',
+    showApartments: 'Смотреть квартиры на этаже'
+  }
+
+  changeFloorUp = () => {
+    if (this.state.currentFloor == 18) return; 
+    this.setState({currentFloor: this.state.currentFloor + 1})
+  }
+
+  changeFloorDown = () => {
+    if (this.state.currentFloor == 2) return; 
+    this.setState({currentFloor: this.state.currentFloor - 1})
   }
 
   render() {
@@ -13,6 +25,21 @@ export default class Main extends React.Component {
       <main className='main-content'>
         <div className='home-image'>
           <ReactSVG src='home.svg' />
+        </div>
+        <div className="choice-floor">
+          <h3 className="choice-floor-head">{this.state.choiceFloorHead}</h3>
+          <div className="manage-floor">
+            <button 
+              type="button" 
+              className='manage-floor-button manage-floor-button_up'
+              onClick={this.changeFloorUp}></button>
+            <span className='current-floor'>{this.state.currentFloor}</span>
+            <button 
+              type="button" 
+              className='manage-floor-button manage-floor-button_down'
+              onClick={this.changeFloorDown}></button>
+          </div>
+          <button type='button' className='show-popup'>{this.state.showApartments}</button>
         </div>
       </main>
     )
